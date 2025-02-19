0bccfa7d1cfd254a1184d946617b72da0bd9d92a
prasad-22 <durgavaraprasadvoonna@gmail.com>
2025-02-19 12:15:05 +0530

    created js file

---
+++ basic/app.js
@@ -0,0 +1,11 @@
+
+let imageElement = document.getElementById("x")
+
+function sun(){
+    imageElement.src="./images/sun.png";
+    document.body.style.background="white"
+}
+function moon(){
+    imageElement.src="./images/moon.png"
+    document.body.style.background="black"
+}
